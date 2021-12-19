d5533ed2285c0e5e9b3dfe265617fab4ab240a0c
byIx <40878600+byIx@users.noreply.github.com>
2021-12-19 18:59:38 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1639917000, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2614-k4-yandex-newyear-garland\/" }
+{ "version":"1.0.1", "longversion":10001, "released":1639917000, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2614-k4-yandex-newyear-garland\/" }
